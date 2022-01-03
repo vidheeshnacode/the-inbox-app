5c92a349a561e841d911858a61f4120a8eba45ec
Vidheesh Nacode <vidheeshnacode@hedwig.local>
2022-01-03 15:34:58 -0500

    Add AstraDB Connection

--- src/main/java/io/vid/InboxApp.java
+++ src/main/java/io/vid/InboxApp.java
@@ -2,10 +2,14 @@ package io.vid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -15,9 +19,9 @@ public class InboxApp {
 		SpringApplication.run(InboxApp.class, args);
 	}
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 }
